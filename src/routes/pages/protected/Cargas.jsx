1dbccd26fb3a8078c44f4d9c17ca6f566897b725
martincamer <martincamer011@gmail.com>
2024-09-17 14:42:00 -0300

    asd

--- src/routes/pages/protected/Cargas.jsx
+++ src/routes/pages/protected/Cargas.jsx
@@ -4,8 +4,8 @@ import { useObtenerId } from "../../../helpers/useObtenerId";
 import { FaSearch } from "react-icons/fa";
 import { formatearFecha } from "../../../helpers/formatearFecha";
 import { useForm } from "react-hook-form";
-import client from "../../../api/axios";
 import { toast } from "react-toastify";
+import client from "../../../api/axios";
 
 export const Cargas = () => {
   const { cargas } = useCargasContext();
@@ -327,6 +327,154 @@ const ModalNuevoRegistro = () => {
   );
 };
 
+const ModalEditarRegistro = ({ idObtenida }) => {
+  const { register, handleSubmit, reset } = useForm();
+  const [items, setItems] = useState([{ detalle: "", cantidad: "" }]);
+
+  const { setCargas } = useCargasContext();
+
+  const onSubmit = async (formData) => {
+    try {
+      const ordenData = {
+        ...formData,
+        datos: items, // Añadir los items al objeto que envías
+      };
+
+      const res = await client.put("/cargas", ordenData);
+
+      setCargas(res.data.todasLasCargas);
+
+      reset();
+
+      document.getElementById("my_modal_nueva_carga").close();
+      setItems([{ detalle: "", cantidad: "" }]); // Reiniciar el formulario y los items
+    } catch (error) {
+      console.error("Error creating product:", error);
+    }
+  };
+
+  const handleAddItem = () => {
+    setItems([...items, { detalle: "", cantidad: "" }]); // Añadir una nueva fila vacía
+  };
+
+  const handleItemChange = (index, field, value) => {
+    const updatedItems = [...items];
+    updatedItems[index][field] = value; // Actualizar el valor del campo
+    setItems(updatedItems);
+  };
+
+  const handleRemoveItem = (index) => {
+    const updatedItems = items.filter((_, i) => i !== index); // Remover el item correspondiente
+    setItems(updatedItems);
+  };
+
+  return (
+    <dialog id="my_modal_nueva_carga" className="modal">
+      <div className="modal-box rounded-md max-w-7xl">
+        <form method="dialog">
+          {/* Si hay un botón en el formulario, cerrará el modal */}
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            ✕
+          </button>
+        </form>
+        <h3 className="font-bold text-xl">Cargar nuevo registro</h3>
+        <p className="py-1 text-sm font-medium">
+          En esta sección podras cargar un nuevo registro, de productos, etc.
+        </p>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="mt-3">
+          <div className="font-bold mb-2 text-[#FD454D] text-lg">
+            Datos del registro.
+          </div>
+          <div className="grid grid-cols-4 gap-2">
+            <div className="flex flex-col gap-2 w-auto">
+              <label className="font-bold text-sm">Nombre y apellido</label>
+              <input
+                {...register("nombre_apellido")}
+                className="border px-2 py-1.5 text-sm text-gray-700 rounded-md outline-none focus:border-blue-600 w-auto"
+                placeholder="Escribir el nombre y apellido..."
+              />
+            </div>
+            <div className="flex flex-col gap-2 w-auto">
+              <label className="font-bold text-sm">Numero del remito</label>
+              <input
+                {...register("numero_remito")}
+                className="border px-2 py-1.5 text-sm text-gray-700 rounded-md outline-none focus:border-blue-600 w-auto"
+                placeholder="Escribrir el numero ej: 000-00010"
+              />
+            </div>
+            <div className="flex flex-col gap-2 w-auto">
+              <label className="font-bold text-sm">Destino</label>
+              <input
+                {...register("destino")}
+                className="border px-2 py-1.5 text-sm text-gray-700 rounded-md outline-none focus:border-blue-600 w-auto"
+                placeholder="Escribrir el desitino Ej: Burzcaco"
+              />
+            </div>
+          </div>
+          <div className="mt-5">
+            <div className="font-bold mb-2 text-[#FD454D] text-lg">
+              Cargar nuevos items del registro.
+            </div>
+
+            <div className="border">
+              {items.map((item, index) => (
+                <div key={index} className="grid grid-cols-3 gap-2">
+                  <div className="flex flex-col gap-2 w-auto p-3 border-r">
+                    {/* <label className="font-bold text-sm">
+                      Detalle del producto
+                    </label> */}
+                    <input
+                      value={item.detalle}
+                      onChange={(e) =>
+                        handleItemChange(index, "detalle", e.target.value)
+                      }
+                      className="border-b px-2 py-1.5 text-sm text-gray-700 outline-none  w-auto uppercase"
+                      placeholder="Escribir el detalle del producto..."
+                    />
+                  </div>
+                  <div className="flex flex-col gap-2 w-auto p-3">
+                    {/* <label className="font-bold text-sm">Cantidad</label> */}
+                    <input
+                      value={item.cantidad}
+                      onChange={(e) =>
+                        handleItemChange(index, "cantidad", e.target.value)
+                      }
+                      className="border-b px-2 py-1.5 text-sm text-gray-700 outline-none  w-auto uppercase font-bold"
+                      placeholder="Escribir la cantidad..."
+                    />
+                  </div>
+                  <div className="flex items-center">
+                    <button
+                      type="button"
+                      onClick={() => handleRemoveItem(index)}
+                      className="text-red-500 border px-4 rounded-md border-red-500 text-sm py-1 hover:bg-red-50 transition-all font-semibold"
+                    >
+                      Eliminar
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <button
+              type="button"
+              onClick={handleAddItem}
+              className="mt-3 font-semibold text-sm text-blue-500"
+            >
+              Añadir nuevo item
+            </button>
+          </div>
+          <div className="mt-6">
+            <button className="font-semibold text-sm bg-gray-700 py-1 px-4 rounded-md text-white">
+              Guardar registro
+            </button>
+          </div>
+        </form>
+      </div>
+    </dialog>
+  );
+};
+
 const ModalObservarRegistro = ({ idObtenida }) => {
   const [carga, setCarga] = useState({});
   const [parsedDatos, setParsedDatos] = useState([]);
@@ -392,6 +540,10 @@ const ModalObservarRegistro = ({ idObtenida }) => {
               <span className="font-bold text-blue-500">
                 N° {carga.numero_remito}
               </span>
+            </p>{" "}
+            <p className="font-semibold uppercase text-sm">
+              Destino:{" "}
+              <span className="font-bold text-blue-500">{carga.destino}</span>
             </p>
           </div>
         </div>
